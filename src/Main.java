0ff613ed3ba7dc435946ecadd2931b6d548394e7
Daniel Sanche <d.sanche14@gmail.com>
2016-02-25 14:47:35 -0600

    added option to choose between ai players and humans

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,35 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String [] args){
         int boardSize = 5;
+        Scanner reader = new Scanner(System.in);
+        Player whitePlayer;
+        Player blackPlayer;
+        String inputString;
         //initialize game
-        Player whitePlayer = new HumanPlayer(boardSize, false);
-        Player blackPlayer = new HumanPlayer(boardSize, true);
+        do {
+            System.out.println("Is the white player a human? (y/n)");
+            inputString = reader.next();
+        } while(!isValidInput(inputString));
+
+        if(inputString.equals("yes") || inputString.equals("y") || inputString.equals("Y")){
+            whitePlayer = new HumanPlayer(boardSize, false);
+        } else {
+            whitePlayer = new RobotPlayer(boardSize, false);
+        }
+        
+        do {
+            System.out.println("Is the black player a human? (y/n)");
+            inputString = reader.next();
+        } while(!isValidInput(inputString));
+        if(inputString.equals("yes") || inputString.equals("y") || inputString.equals("Y")){
+            blackPlayer = new HumanPlayer(boardSize, true);
+        } else {
+            blackPlayer = new RobotPlayer(boardSize, true);
+        }
+        
         BoardState currentState = new BoardState(boardSize, whitePlayer, blackPlayer);
         
         //run the game loop
@@ -22,6 +47,7 @@ public class Main {
             }
             gameComplete = !(whiteCanMove || blackCanMove);
         }
+        //print out winner information
         System.out.println("\n\n\n----------------");
         currentState.renderState();
         System.out.println("\n Game Complete");
@@ -31,6 +57,11 @@ public class Main {
         } else {
             System.out.println(winner.toString() + " Wins!");
         }
+    }
+    
+    private static Boolean isValidInput(String input){
+        return (input.equals("yes") || input.equals("y") || input.equals("Y") || 
+                input.equals("no") || input.equals("n") || input.equals("N")); 
     }
 
 }
---
+++ src/RobotPlayer.java
@@ -0,0 +1,9 @@
+
+public class RobotPlayer extends Player {
+
+    public RobotPlayer(int numPawns, Boolean isBlack) {
+        super(numPawns, isBlack);
+        // TODO Auto-generated constructor stub
+    }
+
+}
