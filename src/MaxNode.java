34c94a980ae279764c03856858713fc58c09bd78
Daniel Sanche <d.sanche14@gmail.com>
2016-02-25 17:53:33 -0600

    stops looking if it finds the best score possible, returns the current score if there are no successors

--- src/MaxNode.java
+++ src/MaxNode.java
@@ -8,18 +8,25 @@ public class MaxNode extends GameNode {
     }
 
     public int findBestOption(int depth){
+        int maxScorePossible = this.rootState.boardSize -1;
         List<BoardState> successorOptions = this.findSuccessorStates();
-        Iterator<BoardState> it = successorOptions.iterator();
-        int maxVal = 0;
-        while(it.hasNext()){
-            BoardState nextState = it.next();
-            MinNode nextNode = new MinNode(nextState, !this.isBlack);
-            int nextCost = nextNode.findBestOption(depth-1);
-            if(nextCost > maxVal){
-                maxVal = nextCost;
-                this.bestOption = nextState;
+        if(!successorOptions.isEmpty()){
+            Iterator<BoardState> it = successorOptions.iterator();
+            int maxVal = 0;
+            while(it.hasNext() && maxVal<maxScorePossible){
+                BoardState nextState = it.next();
+                MinNode nextNode = new MinNode(nextState, !this.isBlack);
+                int nextCost = nextNode.findBestOption(depth-1);
+                if(nextCost > maxVal){
+                    maxVal = nextCost;
+                    this.bestOption = nextState;
+                }
             }
+            return maxVal;
+        } else {
+            //if there are no other options, return the value of this state
+            TerminalNode lastNode = new TerminalNode(this.rootState, this.isBlack);
+            return lastNode.findBestOption(depth);
         }
-        return maxVal;
     }
 }
--- src/MinNode.java
+++ src/MinNode.java
@@ -9,25 +9,31 @@ public class MinNode extends GameNode {
     
     public int findBestOption(int depth){
         List<BoardState> successorOptions = this.findSuccessorStates();
-        Iterator<BoardState> it = successorOptions.iterator();
-        int minVal = Integer.MAX_VALUE;
-        while(it.hasNext()){
-            int nextCost;
-            BoardState nextState = it.next();
-            GameNode nextNode;
-            if(depth == 0){
-              //terminal node. use base cost
-                nextNode = new TerminalNode(nextState, !this.isBlack);
-            } else {
-                //more levels. Make another max node
-                nextNode = new MaxNode(nextState, !this.isBlack);
+        if(!successorOptions.isEmpty()){
+            Iterator<BoardState> it = successorOptions.iterator();
+            int minVal = Integer.MAX_VALUE;
+            while(it.hasNext()){
+                int nextCost;
+                BoardState nextState = it.next();
+                GameNode nextNode;
+                if(depth == 0){
+                    //terminal node. use base cost
+                    nextNode = new TerminalNode(nextState, !this.isBlack);
+                } else {
+                    //more levels. Make another max node
+                    nextNode = new MaxNode(nextState, !this.isBlack);
+                }
+                nextCost = nextNode.findBestOption(depth);
+                if(nextCost < minVal){
+                    minVal = nextCost;
+                    this.bestOption = nextState;
+                }
             }
-            nextCost = nextNode.findBestOption(depth);
-            if(nextCost < minVal){
-                minVal = nextCost;
-                this.bestOption = nextState;
-            }
+            return minVal;
+        } else {
+            //if there are no other options, return the value of this state
+            TerminalNode lastNode = new TerminalNode(this.rootState, this.isBlack);
+            return lastNode.findBestOption(depth);
         }
-        return minVal;
     }
 }
