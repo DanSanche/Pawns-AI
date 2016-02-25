cd4fb05d191afb897d55d0a76222f44beaa4c757
Daniel Sanche <d.sanche14@gmail.com>
2016-02-25 12:22:26 -0600

    prevents user from selecting pawn with no moves

--- src/HumanPlayer.java
+++ src/HumanPlayer.java
@@ -14,11 +14,21 @@ public class HumanPlayer extends Player {
         Collection<Integer> pawnOptions = currentState.renderPawnOptions(this);
         System.out.println("Which pawn do you want to move?: ");
         int pawnIdx = reader.nextInt();
-        while(!pawnOptions.contains(new Integer(pawnIdx-1))){
-            System.out.println("Please enter a number representing a pawn on the board:");
-            pawnIdx = reader.nextInt();
+        Pawn selectedPawn = null;
+        while(selectedPawn==null){
+            if(!pawnOptions.contains(new Integer(pawnIdx-1))){
+                System.out.println("Please enter a number representing a pawn on the board:");
+                pawnIdx = reader.nextInt();
+            } else {
+                selectedPawn = this.getPawnList().get(pawnIdx-1);
+                if(currentState.nextOptionsForPawn(selectedPawn).size()==0){
+                    selectedPawn = null;
+                    pawnOptions = currentState.renderPawnOptions(this);
+                    System.out.println("No moves available for that pawn. Select another:");
+                    pawnIdx = reader.nextInt();
+                }
+            }
         }
-        Pawn selectedPawn = this.getPawnList().get(pawnIdx-1);
         
         List<Integer> moveOptions = currentState.renderMoveOptions(this, selectedPawn);
         System.out.println("Which move do you want to make?:");
